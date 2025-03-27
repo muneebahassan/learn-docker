6880ecee29a02bbddaa5965ff0f2e92e95334e2f
muneebahassan <theailaboratories@gmail.com>
2025-03-27 22:32:26 +0500

    more project

---
+++ project_interactive-python-app/iterate.py
@@ -0,0 +1,6 @@
+# iterate.py
+print("Welcome to the Iteration App!")
+num = int(input("Enter a number to iterate up to: "))
+for i in range(1, num + 1):
+    print(f"Step {i}")
+print("Done!")
--- project_todolist/todo.sh
+++ project_todolist/todo.sh
@@ -3,4 +3,5 @@
 echo "To-DO List: " > tasks.txt
 echo "1. Learn Docker" >> tasks.txt
 echo "2. Build Projects" >> tasks.txt
+echo "3. Give an interview" >> tasks.txt
 cat tasks.txt
